5e3cbe6e6b58f49babc47abcaed6637a7abcb0e5
Meghan O'Halloran <meghaneohalloran@gmail.comuser.name meghanohallorangit pushcode>
2025-01-28 15:03:30 -0700

    git push 3:03MST

---
+++ index.js
@@ -0,0 +1,42 @@
+const state = {
+  eventName: []
+}
+
+const getPartyInformation = async() => {
+  try 
+  const response = await fetch(`https://fsa-crud-2aa9294fe819.herokuapp.com/api`);
+  const responseObject = await response.json();
+  const addDetails
+  console.log
+
+string.slice(indexStart );
+string.slice(indexStart, indexEnd);
+
+  {
+   id: 1,
+   name: "Event Name",
+   description: "This is a description of the event.",
+   date: "2021-09-30T00:00:00.000Z", // Date ISO string
+   location: "123 Street"
+   }
+}
+
+
+// Build your website according to this user story:
+
+// A user enters the website and finds a list of the names, dates, times, locations, and descriptions 
+// of all the parties that are happening.
+
+// ***** NOT THIS UNLESS EXTRA TIME: There is also a form that allows the user to enter information about a new party
+// that they want to schedule. After filling out the form and submitting it, the user observes their 
+// party added to the list of parties.
+
+// DO NOT USE  THIS...DO NOT EDIT OR CHANGE ANOTHER PERSONS CODE
+//  DELETE /<resource>/<id>
+//  Attempts to delete the object identified by the given id. Sends status code 204 if successful.
+
+// Create a new repository named party-planner and push it to GitHub.
+
+// Read the API documentationLinks to an external site.. You will be working with the /events endpoint.
+
+// Plan your code based on the user story.
